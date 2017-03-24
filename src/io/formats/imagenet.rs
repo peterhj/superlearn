06451648625ee80c21fa3038dc31f8dd24e15005
Peter Jin <peterhaijin@gmail.com>
2017-03-24 13:42:30 -0700

    Use "extar" to implement a faster ImageNet reader.

--- src/io/formats/imagenet.rs
+++ src/io/formats/imagenet.rs
@@ -1,14 +1,44 @@
 use io::*;
 
 use csv::{Reader as CsvReader};
+use extar::{TarBufferExt, TarBuffer};
 use sharedmem::{MemoryMap, SharedMem};
 use tar::{Archive};
 
 //use byteorder::{ReadBytesExt, BigEndian};
 use std::collections::{HashMap};
 use std::fs::{File};
+//use std::io::{Read, Seek, BufRead, BufReader, Cursor, SeekFrom, Result as IoResult};
 use std::io::{BufRead, BufReader, Cursor};
 use std::path::{PathBuf};
+
+/*pub struct BytesCursor<A> {
+  inner:    Cursor<A>,
+}
+
+impl<A> BytesCursor<A> {
+  pub fn new(inner: Cursor<A>) -> Self {
+    BytesCursor{inner: inner}
+  }
+}
+
+impl<A> AsRef<[u8]> for BytesCursor<A> where A: AsRef<[u8]> {
+  fn as_ref(&self) -> &[u8] {
+    self.inner.get_ref().as_ref()
+  }
+}
+
+impl<A> Seek for BytesCursor<A> where A: AsRef<[u8]> {
+  fn seek(&mut self, style: SeekFrom) -> IoResult<u64> {
+    self.inner.seek(style)
+  }
+}
+
+impl<A> Read for BytesCursor<A> where A: AsRef<[u8]> {
+  fn read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
+    self.inner.read(buf)
+  }
+}*/
 
 #[derive(Clone)]
 pub struct WordnetIlsvrc2012IdMap {
@@ -92,7 +122,7 @@ impl Ilsvrc2012TrainData {
 
     let mut entries = Vec::new();
 
-    let reader = Cursor::new(archive_buf.clone());
+    /*let reader = Cursor::new(archive_buf.clone());
     let mut archive = Archive::new(reader);
     for (wnid_idx, wnid_entry) in archive.entries().unwrap().enumerate() {
       let wnid_entry = wnid_entry.unwrap();
@@ -111,6 +141,49 @@ impl Ilsvrc2012TrainData {
         assert!(wnid_pos + im_pos + im_size <= wnid_pos + wnid_size);
 
         let im_path = im_entry.header().path().unwrap().into_owned();
+        let im_path_toks: Vec<_> = im_path.to_str().unwrap().splitn(2, ".").collect();
+        let im_stem_toks: Vec<_> = im_path_toks[0].splitn(2, "_").collect();
+        let im_wnid = im_stem_toks[0].to_owned();
+
+        let im_id = *wnid_id_map.wnid_to_id.get(&im_wnid).unwrap();
+        assert!(im_id >= 1);
+        let im_label = (im_id - 1) as u32;
+        assert!(im_label < 1000);
+
+        let entry = Entry{
+          offset:   (wnid_pos + im_pos) as usize,
+          length:   im_size as usize,
+          label:    Some(im_label),
+        };
+        /*if wnid_idx == 0 && im_idx < 5 {
+          println!("DEBUG: imagenet data: offset: {} length: {} label: {:?}",
+              entry.offset, entry.length, entry.label);
+        }*/
+        entries.push(entry);
+      }
+    }*/
+
+    let reader = Cursor::new(archive_buf.clone());
+    let mut archive = TarBuffer::new(reader);
+    for (wnid_idx, wnid_entry) in archive.raw_entries().unwrap().enumerate() {
+      let wnid_entry = wnid_entry.unwrap();
+      let wnid_pos = wnid_entry.raw_file_position();
+      let wnid_size = wnid_entry.file_size();
+      //assert_eq!(wnid_size, wnid_entry.header().size().unwrap());
+
+      //let mut wnid_archive = Archive::new(wnid_entry);
+      let mut wnid_archive = TarBuffer::new(Cursor::new(archive_buf.slice_v2(wnid_pos as usize .. (wnid_pos + wnid_size) as usize)));
+      for (im_idx, im_entry) in wnid_archive.raw_entries().unwrap().enumerate() {
+        let im_entry = im_entry.unwrap();
+        let im_pos = im_entry.raw_file_position();
+        let im_size = im_entry.file_size();
+        //assert_eq!(im_size, im_entry.header().size().unwrap());
+        assert!(wnid_pos + im_pos >= wnid_pos);
+        assert!(wnid_pos + im_pos < wnid_pos + wnid_size);
+        assert!(wnid_pos + im_pos + im_size <= wnid_pos + wnid_size);
+
+        //let im_path = im_entry.header().path().unwrap().into_owned();
+        let im_path = im_entry.path.clone();
         let im_path_toks: Vec<_> = im_path.to_str().unwrap().splitn(2, ".").collect();
         let im_stem_toks: Vec<_> = im_path_toks[0].splitn(2, "_").collect();
         let im_wnid = im_stem_toks[0].to_owned();
@@ -175,7 +248,7 @@ impl Ilsvrc2012ValidData {
 
     let mut entries = Vec::new();
 
-    let reader = Cursor::new(archive_buf.clone());
+    /*let reader = Cursor::new(archive_buf.clone());
     let mut archive = Archive::new(reader);
     for (_, im_entry) in archive.entries().unwrap().enumerate() {
       let im_entry = im_entry.unwrap();
@@ -184,6 +257,36 @@ impl Ilsvrc2012ValidData {
       assert_eq!(im_size, im_entry.header().size().unwrap());
 
       let im_path = im_entry.header().path().unwrap().into_owned();
+      let im_path_toks: Vec<_> = im_path.to_str().unwrap().splitn(2, ".").collect();
+      let im_stem_toks: Vec<_> = im_path_toks[0].splitn(3, "_").collect();
+      assert_eq!("val", im_stem_toks[1]);
+      let im_rank_tok = im_stem_toks[2].to_owned();
+
+      let im_rank: i64 = im_rank_tok.parse().unwrap();
+      assert!(im_rank >= 1);
+      let im_idx = (im_rank - 1) as usize;
+      let im_id = truth.ids[im_idx];
+      assert!(im_id >= 1);
+      let im_label = (im_id - 1) as u32;
+      assert!(im_label < 1000);
+
+      let entry = Entry{
+        offset:   im_pos as usize,
+        length:   im_size as usize,
+        label:    Some(im_label),
+      };
+      entries.push(entry);
+    }*/
+
+    let reader = Cursor::new(archive_buf.clone());
+    let mut archive = TarBuffer::new(reader);
+    for (_, im_entry) in archive.raw_entries().unwrap().enumerate() {
+      let im_entry = im_entry.unwrap();
+      let im_pos = im_entry.raw_file_position();
+      let im_size = im_entry.file_size();
+      //assert_eq!(im_size, im_entry.header().size().unwrap());
+
+      let im_path = im_entry.path.clone();
       let im_path_toks: Vec<_> = im_path.to_str().unwrap().splitn(2, ".").collect();
       let im_stem_toks: Vec<_> = im_path_toks[0].splitn(3, "_").collect();
       assert_eq!("val", im_stem_toks[1]);

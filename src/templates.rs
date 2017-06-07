7d6fb65c9dbc2a75865b43d7ea66490e2cf7fd8e
Peter Jin <peterhaijin@gmail.com>
2017-06-07 12:09:46 -0700

    Use `AVar` trait in model templates instead of `ArrayOp`.

--- src/templates.rs
+++ src/templates.rs
@@ -22,10 +22,10 @@ use std::rc::{Rc};
 
 pub struct CategoricalNLLLoss<Input, Label, Prob, Loss> {
   pub obj:          Rc<AutodiffSink>,
-  pub input:        Rc<ArrayOp<Input>>,
-  pub label:        Rc<ArrayOp<Label>>,
-  pub prob:         Rc<ArrayOp<Prob>>,
-  pub loss:         Rc<ArrayOp<Loss>>,
+  pub input:        Rc<AVar<AData<Input>>>,
+  pub label:        Rc<AVar<AData<Label>>>,
+  pub prob:         Rc<AVar<AData<Prob>>>,
+  pub loss:         Rc<AVar<AData<Loss>>>,
   pub input_vars:   VarSet,
   pub label_vars:   VarSet,
   pub prob_vars:    VarSet,
@@ -44,9 +44,9 @@ pub struct BatchNormIo {
 }
 
 pub struct ClassifierIo<In> {
-  pub x:        Rc<ArraySrc<In>>,
-  pub y_label:  Rc<ArraySrc<Batch<u32>>>,
-  pub y:        Rc<ArrayOp<BatchArray1d<f32>>>,
+  pub x:        Rc<SrcOp<In>>,
+  pub y_label:  Rc<SrcOp<Batch<u32>>>,
+  pub y:        Rc<AVar<AData<BatchArray1d<f32>>>>,
 }
 
 pub struct Classifier<In> {
@@ -68,9 +68,9 @@ impl<In> Classifier<In> {
 }
 
 pub struct RegressorIo<In, Out> {
-  pub x:        Rc<ArraySrc<In>>,
-  pub y_target: Rc<ArraySrc<Out>>,
-  pub y:        Rc<ArrayOp<Out>>,
+  pub x:        Rc<SrcOp<In>>,
+  pub y_target: Rc<SrcOp<Out>>,
+  pub y:        Rc<AVar<AData<Out>>>,
 }
 
 pub struct Regressor<In, Out> {
